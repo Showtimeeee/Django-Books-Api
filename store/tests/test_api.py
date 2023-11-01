2c7b4c24093548ad3a21bd3221b6fcce69585742
Showtimeeeeexitgit config --global user.email sipovichit@yandex.rugit config --global user.name Showtimeeee <sipovichit@yandex.ru>
2023-11-02 00:10:54 +0300

    view search fields, test serializer

--- store/tests/test_api.py
+++ store/tests/test_api.py
@@ -1,13 +1,27 @@
 from django.urls import reverse
+from rest_framework import status
 from rest_framework.test import APITestCase
 
 from store.models import Book
+from store.serializers import BooksSerializer
 
 
 class BooksApiTestCase(APITestCase):
+    def setUp(self):
+        self.book_1 = Book.objects.create(name='Test book 1', price=25, author_name='Author 1')
+        self.book_2 = Book.objects.create(name='Test book 2', price=35, author_name='Author 2')
+        self.book_3 = Book.objects.create(name='Test book 3', price=50, author_name='Author 3')
+
     def test_get(self):
-        book_1 = Book.object.create(name='Test book 1', price=25)
         url = reverse('book-list')
-        print(url)
         response = self.client.get(url)
-        print(response)
+        serializer_data = BooksSerializer([self.book_1, self.book_2, self.book_3], many=True).data
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.assertEqual(serializer_data, response.data)
+
+    def test_search(self):
+        url = reverse('book-list')
+        response = self.client.get(url, data={'search': 'Author 1'})
+        serializer_data = BooksSerializer([self.book_1, self.book_2, self.book_3], many=True).data
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.assertEqual(serializer_data, response.data)
